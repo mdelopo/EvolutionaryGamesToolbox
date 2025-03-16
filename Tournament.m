f1ad370fa36f22deff27f2a8f90347837981ac7f
mdelopo <87904792+mdelopo@users.noreply.github.com>
2025-03-16 02:29:13 +0200

    Update Tournament.m
    Added the ability to select players' strategies when running the tournament.

--- Tournament.m
+++ Tournament.m
@@ -4,8 +4,18 @@ clc;
 %prompt = "Define game bimatrix. ";
 % = input(prompt);
 
+strList= {'cooperate','defect','tit4tat','grim'};
+funList = cellfun(@str2func,strList,'uniformOutput',false);
+
 prompt = "How many rounds will the tournament be? ";
 rounds = input(prompt);
+prompt = "Which strategy will the first player follow? ";
+strategy1 = funList{find(strcmp(strList,input(prompt,'s')))};
+prompt = "Which strategy will the second player follow? ";
+strategy2 = funList{find(strcmp(strList,input(prompt,'s')))};
+
+
+
 
 flags = [false, false]; % Flags for defection by either player
 
@@ -13,22 +23,23 @@ game = zeros(2, rounds);
 player = 1;
 for round = 1:rounds
     if player == 1 
-        game(1, round) = grim(player, round, game, flags);
+        game(1, round) = player(1).strategy(player, round, game, flags);
         if game(1, round) == 'D'
             flags(1) = true;
         end
         player = 2;
     end;
     if player == 2
-        game(2, round) = tit4tat(player, round, game, flags);
+        game(2, round) = strategy2(player, round, game, flags);
         if game(2, round) == 'D'
             flags(2) = true;
         end
         player = 1;
-    end;
-end;
+    end
+end
 
-game
+fprintf('%s: %s\n',char(strategy1), game(1,:));
+fprintf('%s: %s\n',char(strategy2), game(2,:));
 
 function decision = cooperate(player, round, game, flags)
 decision = 'C';
