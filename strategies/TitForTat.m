1a61cf6dfed330cfd786efdde48078a9f96222d2
mdelopo <87904792+mdelopo@users.noreply.github.com>
2025-03-27 00:01:08 +0200

    Fixed TitForTat once again

--- strategies/TitForTat.m
+++ strategies/TitForTat.m
@@ -1,7 +1,7 @@
 function decision = TitForTat(game, player)
-if  game(player,1)==0
-    decision = 1;
-else
-    decision = game(3 - player, find(game(3-player,:),1,'last'));
-end
+    if  game(player,1)==0
+        decision = 1;
+    else
+        decision = game(3 - player, find(game(player,:),1,'last'));
+    end
 end
