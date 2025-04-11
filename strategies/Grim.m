fb7c14a5d1466db32aa44100533d330714eb6794
mdelopo <87904792+mdelopo@users.noreply.github.com>
2025-04-11 21:24:26 +0300

    Fixed Grim Strategy

--- strategies/Grim.m
+++ strategies/Grim.m
@@ -1,5 +1,5 @@
 function Move = Grim(History)
-    if any(History(find(History(:, 1),1,'last'), 2)==2)
+    if any(History(1:nnz(History(:,1)), 2)==2)
         Move = 2;
     else
         Move = 1;
