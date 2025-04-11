8e428e56cf8668d4a937bc5fd905ecaeeb57bea6
mdelopo <87904792+mdelopo@users.noreply.github.com>
2025-04-11 21:11:01 +0300

    Included new strategies in example script

--- example.m
+++ example.m
@@ -1,5 +1,5 @@
 B = [3 1; 4 2];
 T = 10;
-Strategies = ["All_D", "All_C", "Grim", "TitForTat"];
-Pop = [10 10 10 10];
+Strategies = ["All_D", "All_C", "Grim", "TitForTat", "Alternator", "SneakyTitForTat", "SpitefulTitForTat", "TwoTitsForTat", "Prober", "Detective", "TitForTwoTats","Cycler"];
+Pop = [10 10 10 10 10 10 10 10 10 10 10 10];
 scores = Axel(B, Strategies, Pop, T)
