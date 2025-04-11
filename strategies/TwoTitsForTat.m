5880cb1fbce483ca5b8df2709f4e11b24334fb43
mdelopo <87904792+mdelopo@users.noreply.github.com>
2025-04-11 21:10:08 +0300

    Changed based on Axelrod source

--- strategies/TwoTitsForTat.m
+++ strategies/TwoTitsForTat.m
@@ -1,26 +1,11 @@
 function Move = TwoTitsForTat(History)
-    
-    persistent punishment
-
-    if isempty(punishment)
-        punishment = 0; %tracks how many times we need to defect
-    end
-   
-    % First move: cooperate
-    if History(1,1) == 0
-        Move = 1;
-        return;
-    end
-
-    % Check if opponent defected last round
-    if History(find(History(:,1), 1, 'last'), 2) == 2  % opponent defected
-        punishment = punishment + 2;
-    end
-
-    if punishment > 0
-        Move = 2; % defect
-        punishment = punishment - 1;
-    else
-        Move = 1; % cooperate
-    end
+% A player starts by cooperating and replies to each defect by two defections.
+round = nnz(History(:,1)) +1;
+if  round<3
+    Move = TitForTat(History);
+elseif History(round-1,2)==2 || History(round-2,2)==2
+    Move = 2;
+else
+    Move=1;
+end
 end
