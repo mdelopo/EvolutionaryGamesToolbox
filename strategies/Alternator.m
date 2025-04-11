50c6495b84d56d76bdabcc6cfa165b4b779128cd
mdelopo <87904792+mdelopo@users.noreply.github.com>
2025-04-11 21:09:41 +0300

    Fixed improper alternation

--- strategies/Alternator.m
+++ strategies/Alternator.m
@@ -1,18 +1,11 @@
 function Move = Alternator(History)
 %Alternates between cooperating and defecting
 %Starts by cooperating in the first round
-    
-% First move: cooperate
-    if History(1,1) == 0
-        Move = 1;
-        return;
-    end 
-% Count how many actual moves have been made
-    rounds_played = nnz(History(:,1) ~= 0); %ignores empty or initialized rounds 
 
-    if mod(rounds_played, 2) == 0
-        Move = 2;  
-    else
-        Move = 1; 
-    end
+round = nnz(History(:,1))+1;
+if mod(round, 2) == 0
+    Move = 2;
+else
+    Move = 1;
+end
 end
