0925d205b3c069a57436e50b6f9602df348ada1e
mdelopo <87904792+mdelopo@users.noreply.github.com>
2025-05-10 18:01:17 +0300

    Created function to plot three graphs in one figure

--- matlab/example.m
+++ matlab/example.m
@@ -20,19 +20,23 @@ Strategies = ["per_ddc", "Alternator", "soft_majo"];
 POP0 = [100; 100; 100];
 T = 1000;
 J = 90;
-[POP, BST] = TourSimFit(B, Strategies, POP0, T, J);
-% Plot Population of Strategies Over Generations
-fig = plotPopulationOfStrategiesOverGenerations(Strategies, POP,"Defectors may be strong");
-%% 1st case: Monotonous Convergence
+[POP1] = TourTheFit(B, Strategies, POP0, T, J);
+[POP2] = TourSimFit(B, Strategies, POP0, T, J);
+[POP3] = TourSimFit(B, Strategies, POP0, T, J,true);
+% Plot Populations of Strategies Over Generations
+fig = plotPopulationsOfStrategiesOverGenerations(Strategies, POP1, POP2,POP3,"Defectors may be strong");
+%% 1st case: Monotonous Convergence triple plots
 clear; clc;
 B = [3 0; 5 1];
 Strategies = ["Grim", "TitForTat", "Alternator"];
 POP0 = [100; 100; 100];
 T = 1000;
 J = 10;
-[POP, BST] = TourSimFit(B, Strategies, POP0, T, J);
-% Plot Population of Strategies Over Generations
-fig = plotPopulationOfStrategiesOverGenerations(Strategies, POP,"Monotonous Convergence");
+[POP1] = TourTheFit(B, Strategies, POP0, T, J);
+[POP2] = TourSimFit(B, Strategies, POP0, T, J);
+[POP3] = TourSimFit(B, Strategies, POP0, T, J,true);
+% Plot Populations of Strategies Over Generations
+fig = plotPopulationsOfStrategiesOverGenerations(Strategies, POP1, POP2,POP3,"Monotonous Convergence");
 
 %% 2nd case: Attenuated oscillatory movements
 clear; clc;
@@ -41,9 +45,11 @@ Strategies = ["per_ccd", "per_ddc", "soft_majo"];
 POP0 = [450; 1000; 100];
 T = 1000;
 J = 420;
-[POP, BST] = TourSimFit(B, Strategies, POP0, T, J);
-% Plot Population of Strategies Over Generations
-fig = plotPopulationOfStrategiesOverGenerations(Strategies, POP,"Attenuated oscillatory movements");
+[POP1] = TourTheFit(B, Strategies, POP0, T, J);
+[POP2] = TourSimFit(B, Strategies, POP0, T, J);
+[POP3] = TourSimFit(B, Strategies, POP0, T, J,true);
+% Plot Populations of Strategies Over Generations
+fig = plotPopulationsOfStrategiesOverGenerations(Strategies, POP1, POP2,POP3,"Attenuated oscillatory movements");
 %% 3rd case: Periodic movements
 clear; clc;
 B = [3 0; 5 1];
@@ -51,9 +57,11 @@ Strategies = ["per_ccd", "per_ddc", "soft_majo"];
 POP0 = [300; 200; 100];
 T = 1000;
 J = 1000;
-[POP, BST] = TourSimFit(B, Strategies, POP0, T, J);
-% Plot Population of Strategies Over Generations
-fig = plotPopulationOfStrategiesOverGenerations(Strategies, POP,"Periodic movements");
+[POP1] = TourTheFit(B, Strategies, POP0, T, J);
+[POP2] = TourSimFit(B, Strategies, POP0, T, J);
+[POP3] = TourSimFit(B, Strategies, POP0, T, J,true);
+% Plot Populations of Strategies Over Generations
+fig = plotPopulationsOfStrategiesOverGenerations(Strategies, POP1, POP2,POP3,"Periodic movements");
 %% 4th case: Increasing oscillations
 clear; clc;
 B = [3 0; 4.72 1];
@@ -61,9 +69,11 @@ Strategies = ["per_ccd", "per_ddc", "soft_majo"];
 POP0 = [300; 400; 200];
 T = 1000;
 J = 450;
-[POP, BST] = TourSimFit(B, Strategies, POP0, T, J);
-% Plot Population of Strategies Over Generations
-fig = plotPopulationOfStrategiesOverGenerations(Strategies, POP,"Increasing oscillations");
+[POP1] = TourTheFit(B, Strategies, POP0, T, J);
+[POP2] = TourSimFit(B, Strategies, POP0, T, J);
+[POP3] = TourSimFit(B, Strategies, POP0, T, J,true);
+% Plot Populations of Strategies Over Generations
+fig = plotPopulationsOfStrategiesOverGenerations(Strategies, POP1, POP2,POP3,"Increasing oscillations");
 %% 4th case: Sensitivity to CIPD payoff
 clear; clc;
 B = [3 0; 4.6 1];
@@ -71,9 +81,11 @@ Strategies = ["per_ccd", "per_ddc", "soft_majo"];
 POP0 = [300; 244; 100];
 T = 1000;
 J = 500;
-[POP, BST] = TourSimFit(B, Strategies, POP0, T, J);
-% Plot Population of Strategies Over Generations
-fig = plotPopulationOfStrategiesOverGenerations(Strategies, POP,"Increasing oscillations");
+[POP1] = TourTheFit(B, Strategies, POP0, T, J);
+[POP2] = TourSimFit(B, Strategies, POP0, T, J);
+[POP3] = TourSimFit(B, Strategies, POP0, T, J,true);
+% Plot Populations of Strategies Over Generations
+fig = plotPopulationsOfStrategiesOverGenerations(Strategies, POP1, POP2,POP3,"Sensitivity to CIPD payoff");
 %% 5th case: Disordered oscillations
 clear; clc;
 B = [3 0; 5 1];
@@ -81,9 +93,11 @@ Strategies = ["soft_majo", "per_ccccd", "Prober"];
 POP0 = [100; 500; 800];
 T = 1000;
 J = 280;
-[POP, BST] = TourSimFit(B, Strategies, POP0, T, J);
-% Plot Population of Strategies Over Generations
-fig = plotPopulationOfStrategiesOverGenerations(Strategies, POP,"Disordered oscillations");
+[POP1] = TourTheFit(B, Strategies, POP0, T, J);
+[POP2] = TourSimFit(B, Strategies, POP0, T, J);
+[POP3] = TourSimFit(B, Strategies, POP0, T, J,true);
+% Plot Populations of Strategies Over Generations
+fig = plotPopulationsOfStrategiesOverGenerations(Strategies, POP1, POP2,POP3,"Disordered oscillations");
 %% Example showcase of TourTheImi and AnalyzeMarkovChain
 clear; clc;
 B = [3 1; 4 2];
---
+++ matlab/plotPopulationsOfStrategiesOverGenerations.m
@@ -0,0 +1,49 @@
+function fig = plotPopulationsOfStrategiesOverGenerations(Strategies, POP1, POP2, POP3, Title)
+% Determine number of generations
+num_generations = size(POP1, 1);
+% Create generation index
+generations = 1:num_generations;
+
+fig = tiledlayout(3,1);
+
+nexttile
+% Plot each strategy
+for i=1:size(POP1,2)
+    plot(generations, POP1(:,i), 'LineWidth', 2); hold on;
+end
+% Add labels and legend
+subtitle('TourTheFit')
+grid on;
+
+nexttile
+% Plot each strategy
+for i=1:size(POP2,2)
+    plot(generations, POP2(:,i), 'LineWidth', 2); hold on;
+end
+% Add labels and legend
+subtitle('TourSimFit - without compensation')
+grid on;
+
+nexttile
+% Plot each strategy
+for i=1:size(POP3,2)
+    plot(generations, POP3(:,i), 'LineWidth', 2); hold on;
+end
+% Add labels and legend
+subtitle('TourSimFit - with compensation')
+grid on;
+
+t=title(fig,Title);
+subtitle(fig,'Populations of Strategies Over Generations');
+t.FontWeight = 'bold';
+fig.TileSpacing = 'compact';
+fig.Padding = 'compact';
+legend_strategies = replace(Strategies,"_","\_");
+leg = legend(legend_strategies,'Orientation', 'Horizontal');
+leg.Layout.Tile = 'south';
+xlabel(fig, 'Generation');
+ylabel(fig, 'Population');
+set(1, 'units', 'centimeters', 'pos', [0 0 (21-5.1) (29.7-5.1)/1.3])
+
+exportgraphics(fig,'figures/'+Title+'.pdf','ContentType','vector')
+end
