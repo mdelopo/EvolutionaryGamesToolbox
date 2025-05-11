1c8f72448a478912dfe3a6b4a503f1d75afa05ba
mdelopo <87904792+mdelopo@users.noreply.github.com>
2025-05-11 18:27:43 +0300

    Update testing.m

--- matlab/testing.m
+++ matlab/testing.m
@@ -3,6 +3,7 @@ rounds =10;
 History=zeros(rounds,2);
 
 for round = 1:rounds % Play the game
-        History(round,1) = TwoTitsForTat(History);
-        History(round,2) = SneakyTitForTat(flip(History,2));
+        History(round,1) = SneakyTitForTat(History);
+        History(round,2) = soft_majo(flip(History,2));
 end
+History
