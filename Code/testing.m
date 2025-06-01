d37593cf8871062240db2865288fdbe053aeaad3
mdelopo <87904792+mdelopo@users.noreply.github.com>
2025-06-01 20:49:04 +0300

    Removed unnecessary scripts

--- Code/testing.m
+++
@@ -1,9 +0,0 @@
-addpath('./strategies/');
-rounds =10;
-History=zeros(rounds,2);
-
-for round = 1:rounds % Play the game
-        History(round,1) = SneakyTitForTat(History);
-        History(round,2) = soft_majo(flip(History,2));
-end
-History
